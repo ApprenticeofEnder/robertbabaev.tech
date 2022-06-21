452bd6e881d9b941e11fb89d2061f1b332b4a1c9
ApprenticeofEnder <perfknight@gmail.com>
2022-06-21 18:32:23 -0400

    add about me

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -16,28 +16,36 @@
 
 <svelte:head>
 	<title>About</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="About Me" />
 </svelte:head>
 
 <div class="content">
-	<h1>About this app</h1>
-
+	<h1>About Me</h1>
+	<img src="aboutme.jpeg" alt="Robert standing in front of the NorthSec 2022 sign">
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		Hi, my name is Robert, and I'm a third year standing student in a Bachelor of Computer Science
+		at Carleton University. Currently, my focus is in cyber security, which I've been involved in
+		for a few years. I like to think of myself as striking a balance between security and
+		development, where I build tooling and practice secure development in the web apps I build to
+		the best of my ability. In other words, I like being on the purple team.
 	</p>
-
-	<pre>npm init svelte</pre>
-
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		I started my cybersecurity journey in 2019 with, of all things, a business pitch. The idea? 
+		Hack hackers back. Automatically. An idea riddled with flaws, but I was able to convince the 
+		judges of the idea enough that they accepted me into the Hatch 2019 Cohort. It was there that
+		I actually began to study the field in more depth, and realized that in no way was this idea
+		getting off the ground. But that led me to attending a cybersecurity workshop, which got me into 
+		Capture the Flag games, and that encouraged me to practice further.
 	</p>
-
 	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
+		Between June 2021 and June 2022, I tried my hand at competitive Capture the Flag events, with
+		CyberSci Team Canada. I got the opportunity to travel to both Prague, Czechia and Athens, Greece
+		to compete in international CTFs. They were excellent opportunities, which I'm really thankful to CyberSci for,
+		and I was able to see some of Europe for the first time and meet awesome people.
+	</p>
+	<p>
+		Outside of work, I enjoy doing Muay Thai, playing tabletop RPGs such as Lancer,
+		and spending time with my girlfriend (often on the Minecraft server I host).
 	</p>
 </div>
 
@@ -46,5 +54,10 @@
 		width: 100%;
 		max-width: var(--column-width);
 		margin: var(--column-margin-top) auto 0 auto;
+		background-color: rgba(0,0,0,0.7);
+	}
+
+	p {
+		padding-top: 15px;
 	}
 </style>
