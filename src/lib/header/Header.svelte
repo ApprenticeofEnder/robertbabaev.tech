2c8a23f0983e64b9a3904d2e8dbc46fa57a6ae91
ApprenticeofEnder <perfknight@gmail.com>
2022-06-23 21:26:32 -0400

    add articles page

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -16,6 +16,9 @@
 			<li class:active={$page.url.pathname === '/resume'}>
 				<a sveltekit:prefetch href="/resume">Resume</a>
 			</li>
+			<li class:active={$page.url.pathname === '/articles'}>
+				<a sveltekit:prefetch href="/articles">Articles</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ src/routes/articles/index.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	// import { enhance } from '$lib/form';
+	// import { scale } from 'svelte/transition';
+	// import { flip } from 'svelte/animate';
+
+    import type { Article } from '$lib/types';
+
+	export let articles: Article[];
+</script>
+
+<svelte:head>
+	<title>Articles</title>
+	<meta name="description" content="Robert's Articles" />
+</svelte:head>
+
+<div class="articles translucent-black">
+	<h1>Articles</h1>
+
+	{#each articles as article}
+        <h2><a href="{article.link}">{article.title}</a></h2>
+    {/each}
+</div>
+
+<style>
+    h2{
+        width: 100%;
+        padding: 0.25rem;
+    }
+
+	.articles {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+		line-height: 1;
+        padding: 1rem;
+	}
+</style>
