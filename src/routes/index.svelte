3e2bfd16c59dcbc2a73860d01f8a37bacad3d467
ApprenticeofEnder <perfknight@gmail.com>
2022-06-23 20:54:01 -0400

    general error cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -42,20 +42,4 @@
 		width: 100%;
 		text-align: center;
 	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
