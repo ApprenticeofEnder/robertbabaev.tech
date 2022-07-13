b5e51905fc6450d28101f7749329865455eaed3f
Robert Babaev <perfknight@gmail.com>
2022-07-12 22:03:29 -0400

    add site cmd

---
+++ site-cmd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import typer
+import os
+
+app = typer.Typer()
+
+@app.command()
+def createsuperuser():
+    os.system("docker-compose run --rm api python manage.py createsuperuser")
+
+@app.command()
+def start():
+    os.system("docker-compose up -d")
+
+@app.command()
+def stop():
+    os.system("docker-compose down")
+
+@app.command()
+def build():
+    os.system("docker-compose build")
+
+if __name__ == "__main__":
+    app()
