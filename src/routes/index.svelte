c95e514c12c4c3b7457a450050481d2531ac7668
Robert Babaev <perfknight@gmail.com>
2022-07-13 20:37:28 -0400

    update meta tags (also pride flag)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Robert Babaev's Personal Website" />
 </svelte:head>
 
 <section class="translucent-black">
