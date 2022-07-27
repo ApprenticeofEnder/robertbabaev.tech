a3ddb223646162c1187c74d1731d32a04b2b4596
Robert Babaev <perfknight@gmail.com>
2022-07-27 13:55:30 -0400

    add responsiveness to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
@@ -13,10 +12,10 @@
 
 <section class="translucent-black">
 	<div class="flex flex-col justify-center py-48">
-		<div class="grid grid-cols-2 divide-x divide-white">
+		<div class="grid gird-rows-2 md:grid-cols-2 md:divide-x divide-white">
 			<div class="flex flex-col justify-center">
 				<h1>ROBERT BABAEV</h1>
-				<h2>Cyber Security / Development</h2>
+				<h2 class="">Cyber Security / Development</h2>
 			</div>
 			<div class="p-14">
 				<img src="logo.png" alt="Robert Babaev's Logo"/>	
