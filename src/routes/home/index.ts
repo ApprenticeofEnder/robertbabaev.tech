5c64c1f081ccd0c25f763d5b616e233c9345cf84
ApprenticeofEnder <perfknight@gmail.com>
2022-08-31 20:28:53 -0400

    update site to allow dev environment fiddling

--- src/routes/home/index.ts
+++ src/routes/home/index.ts
@@ -1,34 +1,61 @@
 import { api } from '$lib/_api';
 import type { RequestHandler } from './__types';
 import type { Experience, ExperienceSAR, Project, Article, Contact } from '$lib/types';
+import { demoArticles, demoContact, demoExperience, demoProjects } from '$lib/devAids';
+
+function sortExperience(a: Experience, b: Experience){
+	let dateA = new Date(a.start_date);
+	let dateB = new Date(b.start_date);
+	return dateA.getTime() - dateB.getTime();
+}
+
+function sortExperienceSAR(a: ExperienceSAR,b: ExperienceSAR){
+	return a.index - b.index;
+}
 
 export const get: RequestHandler = async () => {
-	const expResponse = await api('get', `experience/`);
-    const projResponse = await api('get', `projects/`);
-    const artiResponse = await api('get', `articles/`);
-    const contResponse = await api('get', `contact/`);
-    
+	// Default to using dev values
+	let experience: Experience[] = demoExperience;
+	let projects: Project[] = demoProjects;
+	let articles: Article[] = demoArticles;
+	let contacts: Contact[] = demoContact;
 
-	if (expResponse.status === 200) {
-		let experience: Experience[] = await expResponse.json();
-        let projects: Project[] = await projResponse.json();
-        let articles: Article[] = await artiResponse.json();
-        let contacts: Contact[] = await contResponse.json();
+	try {
+		const expResponse = await api('get', `experience/`);
+		const projResponse = await api('get', `projects/`);
+		const artiResponse = await api('get', `articles/`);
+		const contResponse = await api('get', `contact/`);
 
-		for(let i = 0; i < experience.length; i++) {
-			experience[i].experience_sars = experience[i].experience_sars.sort((a: ExperienceSAR,b: ExperienceSAR)=>{return a.index - b.index;});
+		if (expResponse.status !== 200) {
+			return {
+				status: expResponse.status
+			};
 		}
+
+		experience = await expResponse.json();
+		projects = await projResponse.json();
+		articles = await artiResponse.json();
+		contacts = await contResponse.json();
+	}
+	catch {
+		if (process.env.SITE_ENV !== 'dev') {
+			return {
+				status: 502
+			};
+		}
+	}
+	finally {
+		for(let i = 0; i < experience.length; i++) {
+			experience[i].experience_sars = experience[i].experience_sars.sort(sortExperienceSAR);
+		}
+		experience = experience.sort(sortExperience).reverse();
 		return {
 			body: {
 				experience,
-                projects,
-                articles,
-                contacts
+				projects,
+				articles,
+				contacts
 			}
 		};
 	}
-
-	return {
-		status: expResponse.status
-	};
 };
