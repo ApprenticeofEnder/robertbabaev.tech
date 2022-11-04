9b802423fad55c5deeef4d931efa891f1fc8d633
ApprenticeofEnder <perfknight@gmail.com>
2022-11-03 20:13:55 -0400

    get to working but not showing on scroll

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
     import { scrollRef, scrollTop } from 'svelte-scrolling';
+	import { onMount } from 'svelte';
 
 	export let scrollRefString: string;
+
+	let section: HTMLElement;
+	onMount(() => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				console.log(entry);
+				if (entry.isIntersecting) {
+					entry.target.classList.add('shown');
+				}
+				else {
+					entry.target.classList.remove('shown');
+				}
+			})
+		})
+		observer.observe(section);
+	});
 </script>
 
-<section use:scrollRef={scrollRefString} class="content translucent-black">
+<section use:scrollRef={scrollRefString} bind:this="{section}" class="hide content translucent-black">
 	<slot />
 </section>
 
@@ -14,5 +31,14 @@
 		max-width: var(--column-width);
 		margin: var(--column-margin-top) auto 0 auto;
 		padding: 1rem;
+	}
+
+	.hide {
+		opacity:0;
+		transition: all 1s;
+	}
+
+	.shown {
+		opacity:1;
 	}
 </style>
