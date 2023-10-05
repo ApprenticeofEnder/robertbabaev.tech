8ab650f142e0a14fe81661c45b0da154fe1f8bdb
ApprenticeofEnder <perfknight@gmail.com>
2023-10-05 01:03:53 -0400

    production ssl script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=($1 $2)
 rsa_key_size=4096
 data_path="./certbot"
 email=$3 # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   for domain in "${domains[@]}"; do
