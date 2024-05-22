436cbec447a4c0378d5fd78104b0a3c1d9c4442c
Robert Babaev <github@robertbabaev.tech>
2024-05-22 01:43:02 -0400

    start About Me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,49 +3,46 @@
 	import Typewriter from 'svelte-typewriter';
 </script>
 
-<div class="text-center">
-	<div class="flex justify-center">
-		<div class="flex flex-initial flex-col divide-none justify-center h-dvh">
-			<div class="grid md:grid-cols-2 md:divide-x divide-text gap-4 mb-4 justify-center">
-				<div class="flex flex-col justify-center">
-					<Typewriter mode="scramble" scrambleDuration={1000}>
-						<Heading tag="h2" color="text-text" customSize="text-2xl sm:text-4xl"
-							>ROBERT BABAEV</Heading
-						></Typewriter
-					>
-				</div>
-				<Img
-					src="/images/enderlogo_v2.png"
-					size="max-w-xs sm:max-w-md"
-					imgClass="mx-auto"
-					alt="sample 1"
-				/>
+<div class="flex justify-center">
+	<div class="flex flex-initial flex-col divide-none justify-center h-dvh">
+		<div class="grid md:grid-cols-2 md:divide-x divide-text gap-4 mb-4 justify-center">
+			<div class="flex flex-col justify-center">
+				<Typewriter mode="scramble" scrambleDuration={1000}>
+					<Heading tag="h2" color="text-text drop-shadow-glow" customSize="text-2xl sm:text-4xl"
+						>ROBERT BABAEV</Heading
+					></Typewriter
+				>
 			</div>
-			<p class="p-4 text-center animate-fade">
-				A security-minded tech professional at the intersection of security, development, and
-				people.
-			</p>
-			<div class="flex justify-center mt-4 animate-fade">
-				<ButtonGroup>
-					<Button
-						class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
-						outline
-						href="/about">About</Button
-					>
-					<Button
-						class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
-						outline>Experience</Button
-					>
-					<Button
-						class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
-						outline>Projects</Button
-					>
-					<Button
-						class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
-						outline>Contact</Button
-					>
-				</ButtonGroup>
-			</div>
+			<Img
+				src="/images/enderlogo_v2.png"
+				size="max-w-xs sm:max-w-md"
+				imgClass="mx-auto"
+				alt="sample 1"
+			/>
+		</div>
+		<p class="p-4 text-center animate-fade">
+			A security-minded tech professional at the intersection of security, development, and people.
+		</p>
+		<div class="flex justify-center mt-4 animate-fade">
+			<ButtonGroup>
+				<Button
+					class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
+					outline
+					href="/about">About</Button
+				>
+				<Button
+					class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
+					outline>Experience</Button
+				>
+				<Button
+					class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
+					outline>Projects</Button
+				>
+				<Button
+					class="text-primary-500 border-primary-500 hover:bg-primary-500 focus:bg-transparent hover:text-black focus:text-white transition"
+					outline>Contact</Button
+				>
+			</ButtonGroup>
 		</div>
 	</div>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,53 @@
-<h1>HI!</h1>
+<script>
+	import { Blockquote, Heading, Li, List } from 'flowbite-svelte';
+	import { AccordionItem, Accordion } from 'flowbite-svelte';
+	import {
+		CheckCircleSolid,
+		CloseCircleSolid,
+		GlobeSolid,
+		LightbulbSolid,
+		QuoteSolid,
+		UserSolid
+	} from 'flowbite-svelte-icons';
+</script>
+
+<div class="flex flex-initial justify-center">
+	<div class="w-1/2">
+		<Heading tag="h1" color="text-primary-500" customSize="text-2xl sm:text-4xl text-center"
+			>About Me</Heading
+		>
+
+		<Accordion
+			class="hover:text-accent-200 hover:bg-transparent"
+			flush
+			activeClass="text-black"
+			inactiveClass="hover:text-accent-200 hover:bg-transparent"
+		>
+			<AccordionItem open class="drop-shadow-none">
+				<span slot="header" class="flex gap-2 text-base text-text drop-shadow-none">
+					<UserSolid class="mt-0.5" />
+					<span>Bio</span>
+				</span>
+				<p class="mb-2 text-text"></p>
+			</AccordionItem>
+			<AccordionItem>
+				<span slot="header" class="flex gap-2 text-base text-text">
+					<LightbulbSolid class="mt-0.5" />
+					<span>Mission</span>
+				</span>
+				<p class="mb-2 text-text">
+					You know, when folks hear cybersecurity, a lot of the time they go "Ah, great, more things
+					I have to interrupt my workflow with." If not those exact words, it's probably some
+					variation of "Urgh."
+				</p>
+			</AccordionItem>
+			<AccordionItem>
+				<span slot="header" class="flex gap-2 text-base text-text">
+					<GlobeSolid class="mt-0.5" />
+					<span>Outside of Tech</span>
+				</span>
+				<p class="mb-2 text-text"></p>
+			</AccordionItem>
+		</Accordion>
+	</div>
+</div>
