363c6a65c524e3258e550d3ae714003476e035db
Robert Babaev <github@robertbabaev.tech>
2024-06-05 03:48:27 -0400

    Memory leak fix (#43)
    * refactoring the website
    
    * additional fiddling
    
    * finish index page + add one integration(?) test
    
    * start About Me
    
    * add additional pages + one more integration test
    
    * finish draft 1 of about me
    
    * remove unused imports
    
    * convert primary component container to main
    
    * fix layout
    
    * scaffold experience gallery
    
    * remove unnecessary file
    
    * switch to pnpm + finish experience page
    
    * finish projects + contact
    
    * update content + formatting
    
    * update content
    
    * add content + formatting + add project link potential
    
    * final update before refactor merge
    
    * add font awesome + move contact buttons into separate component
    
    * hopefully fixed memory issues

---
+++ src/lib/components/ContactButtons.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+import { Button, Alert, Li, List } from 'flowbite-svelte';
+
+import { fade } from 'svelte/transition';
+
+export let emailCopied = false;
+
+const copyEmail = () => {
+	if (emailCopied) {
+		return;
+	}
+	console.log('Initialized Copy');
+	navigator.clipboard
+		.writeText('robert@robertbabaev.tech')
+		.then(() => {
+			emailCopied = true;
+			setTimeout(() => {
+				emailCopied = false;
+			}, 3000);
+		})
+		.catch((err) => {
+			alert(err.message);
+		});
+};
+</script>
+
+<List tag="ul" list="none" class="mb-6 flex flex-wrap items-center justify-center gap-4">
+	<Li>
+		<Button
+			class="me-2 ms-2 border-primary-500 text-primary-500 transition hover:bg-primary-500 hover:text-black focus:bg-transparent focus:text-white focus:ring-0 md:me-3 md:ms-3"
+			outline
+			href="https://github.com/ApprenticeofEnder"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<i class="fa-brands fa-github h-1/2 w-4 md:me-2"></i>
+			<div class="invisible w-0 md:visible md:w-fit">Github</div>
+		</Button>
+	</Li>
+	<Li>
+		<Button
+			class="me-2 ms-2 border-primary-500 text-primary-500 transition hover:bg-primary-500 hover:text-black focus:bg-transparent focus:text-white focus:ring-0 md:me-3 md:ms-3"
+			outline
+			href="https://www.linkedin.com/in/robertbabaev2001/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<i class="fa-brands fa-linkedin h-1/2 w-4 md:me-2"></i>
+			<div class="invisible w-0 md:visible md:w-fit">LinkedIn</div>
+		</Button>
+	</Li>
+	<Li>
+		<Button
+			class="me-2 ms-2 border-primary-500 text-primary-500 transition hover:bg-primary-500 hover:text-black focus:bg-transparent focus:text-white focus:ring-0 md:me-3 md:ms-3"
+			outline
+			on:click={copyEmail}
+		>
+			<i class="fa-solid fa-envelope h-1/2 w-4 md:me-2"></i>
+			<div class="invisible w-0 md:visible md:w-fit">Email</div>
+		</Button>
+	</Li>
+</List>
+{#if emailCopied}
+	<div transition:fade>
+		<Alert
+			dismissable
+			color="none"
+			class="mx-4 border-2 border-primary-500 bg-background text-primary-500 transition md:mx-6"
+			on:click={() => {
+                emailCopied = false;
+            }}
+		>
+			Email copied!
+		</Alert>
+	</div>
+{/if}
