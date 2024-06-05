2be4c461df11a1160a8120d81590eb06fc63c719
Robert Babaev <github@robertbabaev.tech>
2024-06-05 03:34:36 -0400

    hopefully fixed memory issues

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,29 +1,6 @@
 <script lang="ts">
-import Typewriter from 'svelte-typewriter';
-import { AngleRightSolid, GithubBrand, LinkedinBrand, EnvelopeSolid } from 'svelte-awesome-icons';
-import { Heading, Button, Alert, Img, Li, List } from 'flowbite-svelte';
-
-import { fade } from 'svelte/transition';
+import { Heading } from 'flowbite-svelte';
 import ContactButtons from '$lib/components/ContactButtons.svelte';
-
-export let emailCopied = false;
-const copyEmail = () => {
-	if (emailCopied) {
-		return;
-	}
-	console.log('Initialized Copy');
-	navigator.clipboard
-		.writeText('robert@robertbabaev.tech')
-		.then(() => {
-			emailCopied = true;
-			setTimeout(() => {
-				emailCopied = false;
-			}, 3000);
-		})
-		.catch((err) => {
-			alert(err.message);
-		});
-};
 </script>
 
 <main class="flex flex-initial justify-center">
