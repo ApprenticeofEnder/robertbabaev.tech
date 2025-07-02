d2e047133e101333121b49972b7b1fc6b5b5ef8d
Robert Babaev <github@robertbabaev.tech>
2025-07-02 03:49:20 -0400

    fix: cleanup

--- src/mdsvex.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'mdsvex' {
-	interface CompileResponse {
-		code: string;
-	}
-	function compile(src: string): Promise<CompileResponse>;
-}
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,20 +1,2 @@
 <script lang="ts">
-import { Heading } from 'flowbite-svelte';
-import ContactButtons from '$lib/components/ContactButtons.svelte';
 </script>
-
-<main class="flex flex-initial justify-center">
-	<div class="lg:w-3/4">
-		<Heading tag="h1" color="text-primary-500" customSize="text-2xl sm:text-4xl text-center my-4"
-			>Contact</Heading
-		>
-		<div class="flex animate-fade justify-center">
-			<div class="grid lg:w-3/4">
-				<div class="flex justify-center">
-					<p class="text-lg text-text">Want to get in touch?</p>
-				</div>
-				<ContactButtons />
-			</div>
-		</div>
-	</div>
-</main>
