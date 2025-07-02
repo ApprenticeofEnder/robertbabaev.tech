ffdd93521f79d3ca58aba81e7f9baac7c425d793
Robert Babaev <github@robertbabaev.tech>
2025-07-01 22:09:08 -0400

    fix: migrate about section to the about page

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -2,6 +2,8 @@
 	import { TypeWriter } from 'svelte-typewrite';
 
 	import Scramble from '$lib/components/scramble.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
 
 	let designations = ['Security SWE', 'Full Stack SWE', 'Platform Engineer', 'DevOps Engineer'];
 	let currentAffiliation = 'DeepCode';
@@ -37,28 +39,13 @@
 		{@render homepageField(field.name, field.content)}
 	{/each}
 
-	<h2 class="drop-shadow-glow-sm sm:text-3xl">Options</h2>
-</section>
+	<Separator></Separator>
 
-<section class="flex flex-col justify-center gap-8">
-	<h1>Building Digital Fortresses</h1>
-	<p>
-		I have been writing code since I was 13 years old, and securing it since 2020. With a security
-		mindset and a proven track record of learning things on the fly, I can help lock down your
-		codebase, automation systems, and infrastructure.
-	</p>
-	<h1>Flexibility, Creativity, Courage, and Innovation</h1>
-	<p>
-		I've spent years making projects with all manner of technologies, specializing in web and cloud.
-		These range from a structlog integration with FastAPI to a SaaS app in Rails and Next.js that
-		helps students gamify their academic goals. My team and I even built a container runtime for QNX
-		when the platform didn't even support them back at cuHacking 2025 - despite everything seemingly
-		telling us it couldn't be done.
-	</p>
-	<p>
-		If you need your projects to have the same stale life, unfortunately, I can't help you there.
-		That said, I can help breathe new life into anything I work on.
-	</p>
+	<div>
+		<Button href="/resume">Resume</Button>
+		<Button href="/about">About Me</Button>
+		<Button href="/contact">Contact</Button>
+	</div>
 </section>
 
 <section class="flex h-full flex-col justify-center gap-2">
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Hello</h1>
+<section class="flex flex-col justify-center gap-8">
+	<h1>Building Digital Fortresses</h1>
+	<p>
+		I have been writing code since I was 13 years old, and securing it since 2020. With a security
+		mindset and a proven track record of learning things on the fly, I can help lock down your
+		codebase, automation systems, and infrastructure.
+	</p>
+	<h1>Flexibility, Creativity, Courage, and Innovation</h1>
+	<p>
+		I've spent years making projects with all manner of technologies, specializing in web and cloud.
+		These range from a structlog integration with FastAPI to a SaaS app in Rails and Next.js that
+		helps students gamify their academic goals. My team and I even built a container runtime for QNX
+		when the platform didn't even support them back at cuHacking 2025 - despite everything seemingly
+		telling us it couldn't be done.
+	</p>
+	<p>
+		If you need your projects to have the same stale life, unfortunately, I can't help you there.
+		That said, I can help breathe new life into anything I work on.
+	</p>
+</section>
