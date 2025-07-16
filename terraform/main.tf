05d93469e3a37fb3481fb47b9be3b250994e279c
Robert Babaev <github@robertbabaev.tech>
2025-07-16 01:56:42 -0400

    feat: add actual app spec

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,39 @@
 data "digitalocean_domain" "robertbabaev_tech" {
   name = "robertbabaev.tech"
 }
+
+data "digitalocean_project" "personal_website" {
+  name = "Personal Website"
+}
+
+resource "digitalocean_app" "website" {
+  project_id = data.digitalocean_project.personal_website.id
+
+  spec {
+    domain {
+      name = "staging.robertbabaev.tech"
+      type = "PRIMARY"
+      zone = "robertbabaev.tech"
+    }
+
+    name   = "personal-website"
+    region = var.do_region
+
+    static_site {
+      name          = "web-test"
+      build_command = "pnpm build"
+      output_dir    = "/build"
+
+      github {
+        branch         = "overhaul"
+        deploy_on_push = true
+        repo           = "ApprenticeofEnder/robertbabaev.tech"
+      }
+
+      env {
+        key   = "PUBLIC_DEV_RESUME"
+        value = "https://${var.do_bucket}.${var.do_region}.digitaloceanspaces.com/resumes/dev/Robert_Babaev_resume.pdf"
+      }
+    }
+  }
+}
