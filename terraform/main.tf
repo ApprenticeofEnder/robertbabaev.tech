5d45ddfafbdb2927442185da5b4cdf3afaf2ee60
Robert Babaev <github@robertbabaev.tech>
2025-07-16 01:24:49 -0400

    feat: start implementing deployments via terraform

---
+++ terraform/main.tf
@@ -0,0 +1,3 @@
+data "digitalocean_domain" "robertbabaev_tech" {
+  name = "robertbabaev.tech"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "do_bucket" {
+  type    = string
+  default = "robertbabaev-tech"
+}
+
+variable "do_region" {
+  type    = string
+  default = "tor1"
+}
