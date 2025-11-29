4a26e2be274e233bb08638b4f775b19dff07de77
Robert Babaev <github@robertbabaev.tech>
2025-11-28 21:02:20 -0500

    build: initialize devenv (#64)
    * build: initialize devenv
    
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>
    
    * fix: use tree in check-fonts + add roboto
    
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>
    
    * fix: use the right cachix token url
    
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>
    
    * fix: add resumeRoot variable to avoid goofs
    
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>
    
    ---------
    
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

---
+++ devenv.nix
@@ -0,0 +1,107 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  resumeRoot = "${config.git.root}/resume";
+in {
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  cachix.pull = [
+    "rbabaev"
+  ];
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    act
+    git
+    tree
+    gnumake
+
+    # fonts
+    roboto
+    source-sans
+    source-sans-pro
+    font-awesome_6
+  ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    javascript = {
+      enable = true;
+      pnpm = {
+        enable = true;
+        install.enable = true;
+      };
+    };
+    opentofu = {
+      enable = true;
+    };
+    typescript = {
+      enable = true;
+    };
+    typst = {
+      enable = true;
+      fontPaths = [
+        "${pkgs.roboto}/share/fonts/truetype"
+        "${pkgs.source-sans-pro}/share/fonts/truetype"
+        "${pkgs.source-sans}/share/fonts/truetype"
+        "${pkgs.font-awesome_6}/share/fonts/opentype"
+      ];
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  scripts = {
+    compile-resume = {
+      exec = ''
+        set -euxo pipefail
+
+        cd ${resumeRoot}
+
+        ${lib.getExe pkgs.typst} compile --root . ${resumeRoot}/dev/Robert_Babaev_resume.typ
+      '';
+    };
+    check-fonts = {
+      exec = ''
+        tree ${pkgs.source-sans-pro}
+        tree ${pkgs.source-sans}
+        tree ${pkgs.font-awesome_6}
+      '';
+      description = "\tCheck the installation directories of the fonts installed";
+    };
+  };
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    git --version # Use packages
+    typst --version
+    echo pnpm $(pnpm --version)
+    tofu --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
